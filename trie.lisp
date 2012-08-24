800ec662ecb1485e6f59033bd6bb8c4affdea47a
Pierre Thierry <pierre@nothos.net>
2012-08-24 05:58:49 +0200

    Make minor modifications to the trie
    - value is initially NIL
    - add trie-init trivial function
    - make trie-insert a simple function

--- trie.lisp
+++ trie.lisp
@@ -1,13 +1,22 @@
+#|
+
+Trie data structure
+
+|#
+
 (defclass trie ()
   ((children :initform nil :accessor trie-children)
-   (value :initarg :value  :accessor trie-value)))
+   (value :initform nil :accessor trie-value)))
 
-(defmethod trie-insert (trie key value) 
+(defun trie-init ()
+  (make-instance 'trie))
+
+(defun trie-insert (trie key value) 
   (if (zerop (length key))
       (setf (trie-value trie) value)
-      (let ((sub-trie (cif pair (assoc (elt key 0) (trie-children trie))
-			   (cdr pair)
-			   (let ((new-trie (make-instance 'trie)))
-			     (push (cons (elt key 0) new-trie) (trie-children trie))
-			     new-trie))))
+      (let ((sub-trie (if-let (pair (assoc (elt key 0) (trie-children trie)))
+			(cdr pair)
+			(let ((new-trie (make-instance 'trie)))
+			  (push (cons (elt key 0) new-trie) (trie-children trie))
+			  new-trie))))
 	(trie-insert sub-trie (subseq key 1) value))))
