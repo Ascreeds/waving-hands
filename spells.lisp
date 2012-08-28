36c4e929bf844c4f21405ee82a532341334531aa
Pierre THierry <pierre@nothos.net>
2012-08-28 22:16:03 +0200

    Add a few gestures definitions

---
+++ spells.lisp
@@ -0,0 +1,17 @@
+(defconstant +spells-protection+
+  '(("p" . "shield")
+    ("pdwp" . "remove enchantment")
+    ("cW" . "magic mirror")
+    ("wpp" . "counter-spell")
+    ("wws" . "counter-spell")
+    ("cdpw" . "dispel magic")
+    ("dwwfwc" . "raise dead")
+    ("dfw" . "cure light wounds")
+    ("dfpw" . "cure heavy wounds")))
+
+(defconstant +spells-summoning+
+  '(("sfw" . "summon goblin")
+    ("psfw" . "summon ogre")
+    ("fpsfw" . "summon troll")
+    ("wfpsfw" . "summon giant")
+    ("cswws" . "summon elemental")))
